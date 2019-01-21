831ea81b18e5199eee6dd4791e6d47d5014cb1a8
unknown <wuwj2001@gmail.com>
2019-01-21 13:04:07 -0800

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,10 @@ import React, { Component } from 'react';
 
 import '../css/App.css';
 
-import { Title } from './Title';
-import { Question } from './Question';
-
 export class App extends Component {
   render() {
-    return (
-      <div className="app">
-        <Title title="Cawu'uq"/>
-        <Question/>        
-      </div>
-    );
+
+  
   }
 }
 
