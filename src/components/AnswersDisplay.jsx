3df1b5b5599269d41c18da0d3760e08f9237336a
Wei <wuwj2001@gmail.com>
2019-02-06 17:28:13 -0800

    some progress ?

--- src/components/AnswersDisplay.jsx
+++ src/components/AnswersDisplay.jsx
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 
 import '../css/AnswersDisplay.css';
 
+function handleWrongChoice() {
+  console.log(false)
+}
+function handleRightChoice() {
+  console.log(true)
+}
+
 export class AnswerDisplay extends Component {
   render() {
-    const answers_array = this.props.answersArray.map((item, index) => 
-      <AnswerItem text={item} key={index} id={index}/>
-    );
+    const answers_array = this.props.answersArray.map((item, index) => {
+      let clickFunction = handleWrongChoice;
+      if (index === this.props.correctChoice) {
+        clickFunction = handleRightChoice;
+      }
+      return ( 
+        <AnswerItem text={item} key={index} id={index} clickFunction={clickFunction}/>
+      )
+    });
     return (
       <div className="answer_display">
         <ul className="list-group">
@@ -20,7 +33,11 @@ export class AnswerDisplay extends Component {
 class AnswerItem extends Component {
   render() {
     return (
-      <button className="answer_item list-group-item list-group-item-action" id={this.props.id}>
+      <button 
+        className="answer_item list-group-item list-group-item-action" 
+        id={this.props.id}
+        onClick={this.props.clickFunction}
+      >
         {this.props.text}
       </button>
     );
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,54 +1,60 @@
 import React, { Component } from 'react';
 
-import { buildFirebase, getRandomQuestion, getFirebaseSnapshot} from "../clients/firebase";
-
 import { Title } from './Title';
 import { Question } from './Question';
 
 import '../css/App.css';
 
-var firebaseDatabase = buildFirebase();
 
-
-const questionArray = [
-  {
-    choices: [1, 2, 3, 4],
-    correct_choice_index: 3,
-    question_text: "What is 2 x 2?"
-  },
-  {
-    choices: [1, 2, 3, 4],
-    correct_choice_index: 1,
-    question_text: "What is 2 x 1?"
-  },
-  {
-    choices: [1, 2, 3, 4],
-    correct_choice_index: 2,
-    question_text: "What is 2 x 1.5?"
-  },
-]
 
 export class App extends Component {
+
+  questionArray = [
+    {
+      choices: [1, 2, 3, 4],
+      correct_choice_index: 3,
+      question_text: "What is 2 x 2?"
+    },
+    {
+      choices: [1, 2, 3, 4],
+      correct_choice_index: 1,
+      question_text: "What is 2 x 1?"
+    },
+    {
+      choices: [1, 2, 3, 4],
+      correct_choice_index: 2,
+      question_text: "What is 2 x 1.5?"
+    },
+  ]
 
   constructor(props) {
     super(props) 
     this.state = {
-      currentQuestion: "a"
+      currentQuestionIndex: 0
     }
-    getFirebaseSnapshot((snapshot) => {
-      const questions = snapshot
-      this.setState({
-        currentQuestion: getRandomQuestion(questions)
-      })
-    }, "/questions")
   }
+
+  nextQuestion() {
+    this.setState({
+      currentQuestionIndex: this.currentQuestionIndex + 1,
+    });
+  }
+
+  functionArray = [
+    this.nextQuestion(),
+  ]
 
   render() {
     return (
       <div className="container app">
         <Title title="Trivia"/>
-        <hr></hr>
-        <Question questionObject={this.state.currentQuestion}/>
+        <hr/>
+        <Question 
+          questionObject={this.questionArray[this.state.currentQuestionIndex]}
+
+        />
+        <hr/>
+
       </div>
     );
   }
