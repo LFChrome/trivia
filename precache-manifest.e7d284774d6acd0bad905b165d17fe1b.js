8e79024ca5ade614244cdfe91310f5a522297363
Wei <wuwj2001@gmail.com>
2019-03-06 17:07:52 -0800

    Updates

---
+++ precache-manifest.e7d284774d6acd0bad905b165d17fe1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "30964e3390d41baeff0b",
+    "url": "/trivia/static/js/runtime~main.30964e33.js"
+  },
+  {
+    "revision": "de4a1caadcfdee8f4911",
+    "url": "/trivia/static/js/main.de4a1caa.chunk.js"
+  },
+  {
+    "revision": "f26edfd165016aed59de",
+    "url": "/trivia/static/js/1.f26edfd1.chunk.js"
+  },
+  {
+    "revision": "de4a1caadcfdee8f4911",
+    "url": "/trivia/static/css/main.89967bfd.chunk.css"
+  },
+  {
+    "revision": "6015cb7918a4fd7aaabb7a1819ce7f08",
+    "url": "/trivia/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.a638f98d4ce0f723e386f9b5c9319f8f.js"
+  "/trivia/precache-manifest.e7d284774d6acd0bad905b165d17fe1b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/trivia/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
